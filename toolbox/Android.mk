5e1b1d232ef5f4a2fb12df3250d728d4f9a40ca0
Dominggoes Isakh <drjisakh@gmail.com>
2017-02-11 16:04:23 +0100

    Include getprop and setprop for M and higher
    Change-Id: Id41306a29b6c612354db7172ccef256591d41b0c

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -215,7 +215,7 @@ ifneq (,$(filter $(PLATFORM_SDK_VERSION), 21 22 23))
     LOCAL_WHOLE_STATIC_LIBRARIES := $(patsubst %,libtoolbox_%,$(BSD_TOOLS))
 endif
 
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -gt 22; echo $$?),0)
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 23; echo $$?),0)
     # Rule to make getprop and setprop in M trees where toybox normally
     # provides these tools. Toybox does not allow for easy dynamic
     # configuration, so we would have to include the entire toybox binary
